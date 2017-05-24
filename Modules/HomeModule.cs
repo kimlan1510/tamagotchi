53b3f672135c66745990436ee30cf01f34d13f08
Jordan Mysliwiec and kimlan nguyen <jordan+kimlan@>
2017-05-24 16:12:17 -0700

    Get the pages to talk to each other

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,6 +9,18 @@ namespace tamagotchi
     public HomeModule()
     {
       Get["/"] = _ => View["index.cshtml"];
+
+      Get["/view_tamagotchi/{id}"] = parameters => {
+        pet newPet = pet.Find(parameters.id);
+        return View["view_tamagotchi.cshtml", newPet];
+      };
+
+      Get["/pet_feed"] = _ => View["pet_feed.cshtml"];
+
+      Post["/create_pet"] = _ => {
+        pet newPet = new pet(Request.Form["name"],70, 70);
+        return View["create_pet.cshtml", newPet];
+      };
     }
   }
 }
--- Objects/pet.cs
+++ Objects/pet.cs
@@ -1,30 +1,45 @@
 using System.Collections.Generic;
 namespace tamagotchi.Objects
 {
-  public class tamagotchi
+  public class pet
   {
     private string _name;
     private string _food;
     private int _time;
     private string _activity;
-    private int energy = 70;
-    private int attention = 70;
-    public food(string foodType, int time)
+    private int _energy;
+    private int _attention;
+    private int _id;
+    private static List<pet> _yourpets = new List<pet>();
+    public pet(string name, int energy, int attention)
     {
-      SetFood(foodType);
-      SetTime(time);
+      SetName(name);
+      SetEnergy(energy);
+      SetAttention(attention);
+      _yourpets.Add(this);
+      _id = _yourpets.Count;
     }
 
-    public play(string activity, int time)
+    public int GetId()
     {
-      SetActivity(activity);
-      SetTime(time);
+      return _id;
     }
-
-    public sleep(int time)
-    {
-      SetTime(time);
-    }
+    // public food(string foodType, int time)
+    // {
+    //   SetFood(foodType);
+    //   SetTime(time);
+    // }
+    //
+    // public play(string activity, int time)
+    // {
+    //   SetActivity(activity);
+    //   SetTime(time);
+    // }
+    //
+    // public sleep(int time)
+    // {
+    //   SetTime(time);
+    // }
 
     public string GetName()
     {
@@ -34,80 +49,99 @@ namespace tamagotchi.Objects
     public void SetName(string name){
       _name = name;
     }
-    public string GetFood()
+    // public string GetFood()
+    // {
+    //   return _food;
+    // }
+    //
+    // public int GetTime()
+    // {
+    //   return _time;
+    // }
+    //
+    // public string GetActivity()
+    // {
+    //   return _activity;
+    // }
+
+    public int GetEnergy()
     {
-      return _food;
+      return _energy;
     }
 
-    public int GetTime()
+    public int GetAttention()
     {
-      return _time;
+      return _attention;
     }
 
-    public string GetActivity()
+    public void SetEnergy(int energy)
     {
-      return _activity;
+      _energy = energy;
     }
 
-    public void SetFood(string food)
+    public void SetAttention(int attention)
     {
-      _food = food;
+      _attention = attention;
     }
 
-    public void SetTime(int time)
+    public static pet Find(int searchId)
     {
-      _time = time;
+      return _yourpets[searchId - 1];
     }
 
-    public void SetActivity(string activity)
-    {
-      _activity = activity;
-    }
+    // public void SetFood(string food)
+    // {
+    //   _food = food;
+    // }
 
-    public tamagotchi(string name)
-    {
-      SetName(name);
+    // public void SetTime(int time)
+    // {
+    //   _time = time;
+    // }
+    //
+    // public void SetActivity(string activity)
+    // {
+    //   _activity = activity;
+    // }
+    //
+    // public int GetFood()
+    // {
+    //   return _food;
+    // }
+    //
+    // public int GetSleep()
+    // {
+    //   return _sleep;
+    // }
+    //
+    // public int GetPlay()
+    // {
+    //   return int _play;
+    // }
 
-    }
-    public int GetFood()
-    {
-      return _food;
-    }
+    // public static tamagotchi CalEnergy(food, sleep, play, time)
+    // {
+    //   if (energy >= 100) {
+    //     energy = 100;
+    //     energy = food + sleep - play - time;
+    //   }
+    //   else{
+    //     energy = food + sleep - play - time;
+    //   }
+    //   return energy;
+    // }
+    //
+    // public static tamagotchi CalAttention(play)
+    // {
+    //   if (attention >= 100) {
+    //     attention = 100;
+    //     attention = play ;
+    //   }
+    //   else{
+    //     attention = play;
+    //   }
+    //   return attention;
+    // }
 
-    public int GetSleep()
-    {
-      return _sleep;
-    }
-
-    public int GetPlay()
-    {
-      return int _play;
-    }
-
-    public static tamagotchi CalEnergy(food, sleep, play, time)
-    {
-      if (energy >= 100) {
-        energy = 100;
-        energy = food + sleep - play - time;
-      }
-      else{
-        energy = food + sleep - play - time;
-      }
-      return energy;
-    }
-
-    public static tamagotchi CalAttention(play)
-    {
-      if (attention >= 100) {
-        attention = 100;
-        attention = play ;
-      }
-      else{
-        attention = play;
-      }
-      return attention;
-    }
-
-    public static tamagotchi TimeCountDown()
   }
 }
